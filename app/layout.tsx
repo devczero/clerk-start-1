205d00e452c8f70c4227d7d7672acf61f40fe2ed
devczero <devczero@gmail.com>
2025-12-29 16:35:36 +0100

    Revert "upd"
    This reverts commit 13c40744be2176709efebbd772fbb3f323832105.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/Navbar";
-import { ThemeProvider } from "@/contexts/ThemeContext";
 
 const bricolage = Bricolage_Grotesque({
   subsets: ["latin"],
@@ -27,10 +26,8 @@ export default function RootLayout({
         <body
           className={`${bricolage.variable} ${bricolage.variable} antialiased`}
         >
-          <ThemeProvider>
-            <Navbar/>
-            {children}
-          </ThemeProvider>
+          <Navbar/>
+          {children}
         </body>
       </html>
     </ClerkProvider>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,11 +4,9 @@ import Link from "next/link"
 import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs"
 import { Menu, X } from "lucide-react"
 import { useState } from "react"
-import { usePathname } from "next/navigation"
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false)
-    const pathname = usePathname()
 
     const toggleMenu = () => {
         setIsOpen(!isOpen)
@@ -16,11 +14,6 @@ export default function Navbar() {
 
     const closeMenu = () => {
         setIsOpen(false)
-    }
-
-    // Hide navbar on dashboard routes
-    if (pathname?.startsWith('/dashboard')) {
-        return null
     }
 
     return (
@@ -110,7 +103,7 @@ export default function Navbar() {
                                 </SignedIn>
                                 <SignedOut>
                                     <SignInButton>
-                                        <button
+                                        <button 
                                             className="px-4 py-2.5 hover:bg-white/30 bg-white/10 border border-white/20 rounded-xl">
                                             Sign In
                                         </button>
